715210d5af0bbb2a28dca1c8492bb9352eb008ba
Skillbox <bdremb@gmail.com>
2024-04-06 11:25:20 +0300

    refactoring code, fixed some bugs

--- spring-rest-api-news/src/main/java/com/example/spring/spring/restapi/news/web/model/request/NewsFilterRequest.java
+++ spring-rest-api-news/src/main/java/com/example/spring/spring/restapi/news/web/model/request/NewsFilterRequest.java
@@ -2,6 +2,7 @@ package com.example.spring.spring.restapi.news.web.model.request;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,12 +21,18 @@ public class NewsFilterRequest {
     @NotNull
     private Integer pageNumber;
 
+    @NotBlank
     private String category;
 
+    @NotBlank
     private String userName;
 
+    @Min(0)
+    @Max(Integer.MAX_VALUE)
     private Long userId;
 
+    @Min(0)
+    @Max(Integer.MAX_VALUE)
     private Long categoryId;
 
 }
