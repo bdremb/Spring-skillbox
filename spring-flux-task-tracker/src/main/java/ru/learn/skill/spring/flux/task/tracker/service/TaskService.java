4f3c6bd96d6356e91236bfedb654ebd8c5996748
Бобкин Денис <bdremb@gmail.com>
2024-05-25 21:21:46 +0300

    refactoring after review

--- spring-flux-task-tracker/src/main/java/ru/learn/skill/spring/flux/task/tracker/service/TaskService.java
+++ spring-flux-task-tracker/src/main/java/ru/learn/skill/spring/flux/task/tracker/service/TaskService.java
@@ -3,6 +3,7 @@ package ru.learn.skill.spring.flux.task.tracker.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.learn.skill.spring.flux.task.tracker.entity.Task;
@@ -16,6 +17,7 @@ import ru.learn.skill.spring.flux.task.tracker.web.model.response.TaskResponse;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -56,9 +58,10 @@ public class TaskService {
     public Mono<TaskResponse> findById(String id) {
         return taskMapper.toMonoResponse(
                 Mono.zip(
-                        taskRepository.findById(id),
-                        userRepository.findAll().collectList()
-                ).map(data -> taskMapper.toTask(data.getT1(), data.getT2()))
+                                taskRepository.findById(id),
+                                getUsersByTaskId(id, null)
+                        )
+                        .map(data -> taskMapper.toTask(data.getT1(), data.getT2()))
         ).onErrorResume(e -> Mono.just(new TaskResponse()));
     }
 
@@ -66,8 +69,10 @@ public class TaskService {
         return taskMapper.toMonoResponse(
                 taskRepository.findById(id)
                         .flatMap(taskForUpdate -> {
+                            List<String> newUserIds = new ArrayList<>(request.getObserverIds());
+                            newUserIds.addAll(List.of(request.getAssigneeId(), request.getAuthorId()));
+                            final Mono<List<User>> allMonoUsers = getUsersByTaskId(id, newUserIds);
                             final Mono<Task> savedTask = taskRepository.save(taskMapper.toUpdatedTask(taskForUpdate, request));
-                            final Mono<List<User>> allMonoUsers = userRepository.findAll().collectList();
                             return Mono.zip(savedTask, allMonoUsers)
                                     .map(data -> taskMapper.toTask(data.getT1(), data.getT2()));
                         })
@@ -81,7 +86,7 @@ public class TaskService {
                             updatedTask.getObserverIds().add(observerId);
                             return Mono.zip(
                                     taskRepository.save(updatedTask),
-                                    userRepository.findAll().collectList(),
+                                    getUsersByTaskId(id, null),
                                     userRepository.findById(observerId)
                             ).map(data -> {
                                 data.getT3();
@@ -89,6 +94,21 @@ public class TaskService {
                             });
                         })
         ).onErrorResume(e -> Mono.just(new TaskResponse()));
+    }
+
+    private Mono<List<User>> getUsersByTaskId(String taskId, List<String> optionalIds) {
+        return taskRepository.findById(taskId)
+                .flatMap(task -> userRepository.findAllById(getUserIdsByTask(task, optionalIds)).collectList());
+    }
+
+    private Set<String> getUserIdsByTask(Task task, List<String> optionalIds) {
+        final Set<String> userIds = task.getObserverIds();
+        userIds.add(task.getAssigneeId());
+        userIds.add(task.getAuthorId());
+        if (!CollectionUtils.isEmpty(optionalIds) && !optionalIds.contains(null)) {
+            userIds.addAll(optionalIds);
+        }
+        return userIds;
     }
 
     public Mono<Void> deleteById(String id) {
